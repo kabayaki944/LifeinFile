6a6ea373b7687248af04042aa3be037a62a642dc
kabayaki944 <game.yarisugi@icloud.com>
2026-05-28 01:10:48 +0900

    ゴミ箱転送 ドラッグ移動

--- LifeinFile/MainWindow.xaml.cs
+++ LifeinFile/MainWindow.xaml.cs
@@ -1,24 +1,40 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace LifeinFile
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        // マウスの左ボタンを押したままドラッグで移動できるようにする
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            this.DragMove();
+        }
+
+        private void Window_Drop(object sender, DragEventArgs e)
+        {
+            // ドロップされたものが「ファイル」かどうか確認
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                // ドロップされたすべてのファイルを順番に処理する
+                foreach (string file in files)
+                {
+                    // PCから完全に消去せず、安全に「ゴミ箱」へ送る
+                    Microsoft.VisualBasic.FileIO.FileSystem.DeleteFile(
+                        file,
+                        Microsoft.VisualBasic.FileIO.UIOption.OnlyErrorDialogs,
+                        Microsoft.VisualBasic.FileIO.RecycleOption.SendToRecycleBin
+                    );
+                }
+            }
         }
     }
 }
