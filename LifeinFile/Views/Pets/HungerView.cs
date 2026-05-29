cf234f99cde7ed847014bd3f980b9d03273e080b
kabayaki944 <game.yarisugi@icloud.com>
2026-05-29 18:37:26 +0900

    GitHubDeskTopに移行

---
+++ LifeinFile/Views/Pets/HungerView.cs
@@ -0,0 +1,27 @@
+﻿using LifeinFile.Helper;
+using LifeinFile.Models.Pets;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using Views.Windows;
+
+namespace LifeinFile.Views.Pets
+{
+    public class HungerView: IUpdateLate
+    {
+        PetModel _model;
+        PetWindow _window;
+        public HungerView(PetModel model, PetWindow window)
+        {
+            _model = model;
+            _window = window;
+            ProvideUpdate.AddUpdateLate(this);
+        }
+
+        public void OnUpdateLate()
+        {
+            _window.HungerGauge.Value = _model.CurrentHunger;
+        }
+    }
+}
